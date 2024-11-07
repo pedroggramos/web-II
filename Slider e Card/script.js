3350cb5c69036f6ce446055fe27833060258d500
Pedro Ramos <pedrogabrielguimaraesramos178@gmail.com>
2024-11-07 14:34:53 +0000

    aula 07/11 incompleto

---
+++ Slider e Card/script.js
@@ -0,0 +1,57 @@
+// var slides = ['f1.avif', 'f2.jpg', 'f3.jpg', 'f4.webp', 'f5.jpg']
+// var tam = slides.length
+// var statual = 0
+// var tmpslider
+
+// // function trocaAutomatica(){
+// //     statual++
+// //     if(statual >= tam){
+// //         statual = 0
+// //     }
+// //     document.querySelector('#dvSlider').style.backgroundImage=`url('${slides[statual]}')`
+// // }
+
+// function inicialSlider(){
+//     document.querySelector('#dvSlider').style.backgroundImage=`url('${slides[statual]}')`
+//     tmpslider = setInterval(trocaAutomatica, 2000)
+// }
+
+// function parar(){
+//     clearInterval(tmpslider)
+// }
+
+// function troca(dir){
+//     statual += dir
+//     if(statual < 0){
+//         statual = 4
+//     }else if(statual >= tam){
+//         statual = 0
+//     }
+//     document.querySelector('#dvSlider').style.backgroundImage=`url('${slides[statual]}')`
+//         clearInterval(tmpslider)
+// }
+
+// function expandCard(card){
+//     card.style.transform='scale(1.3)'
+//     card.style.boxShadow = '0 8px 15px rgba(0,0,0,0.3)'
+// }
+
+// function shinkCard(card){
+//     card.style.transform='scale(1)'
+//     card.style.boxShadow = 'none'
+// }
+
+
+// function rotateCard(card){
+//     card.style.transform  = 'rotate(90deg)'
+// }
+
+// function resetCard(card){
+//     card.style.transform  = 'rotate(0deg)'
+// }
+
+// function expandCard(cardButton){
+//     cardButton.style.transform ='scale(1.05)'
+//     cardButton.style.boxShadow = '0 4px 8px rgba(0,0,0, 0.3')
+// }
+
